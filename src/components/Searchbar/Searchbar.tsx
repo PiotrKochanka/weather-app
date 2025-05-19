730c1fcf751919c33a092be161942518de26aeea
Pietrek <kochankap@gmail.com>
2025-05-19 13:09:07 +0200

    searchbar styling

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import styles from './searchbar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 interface SearchbarProps{
     // onSearch: (query: string) => void;
@@ -7,10 +10,16 @@ interface SearchbarProps{
 
 const Searchbar: React.FC<SearchbarProps> = ({ placeholder = 'Wpisz miasto...' }) => {
     return(
-        <input
-            type="text"
-            placeholder={placeholder}
-        />
+        <>
+            <input
+                type="text"
+                placeholder={placeholder}
+                className={`${styles.searchbar}`}
+            />
+            <button className={`${styles.searchbar_button}`}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+        </>
     );
 }
 
