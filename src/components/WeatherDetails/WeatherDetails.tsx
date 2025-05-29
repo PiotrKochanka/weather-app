f658fdddfcb7a9e9eb3be46c5bfa94b7e53604a2
Piotr Kochanka <kochankap@gmail.com>
2025-05-29 22:06:28 +0200

    styling

--- src/components/WeatherDetails/WeatherDetails.tsx
+++ src/components/WeatherDetails/WeatherDetails.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useFetchWeatherForecast, { ForecastItem, ForecastData } from '../../hooks/useFetchWeatherForecast';
+import styles from './weatherdetails.module.css';
 
 const WeatherDetails: React.FC = () => {
     const city = "Warsaw";
@@ -14,27 +15,31 @@ const WeatherDetails: React.FC = () => {
     }
 
     return(
-        <div className="weather-container">
+        <div>
         {forecast && (
             <div>
-            <h2>Pogoda {forecast.city.name}</h2>
-            <div className="forecast-list">
+            <div className={`${styles.details_container}`}>
                 {forecast.list.slice(0, 1).map((item, index) => (
-                <div key={index} className="forecast-item">
-                    <p><strong>{item.weather[0].description}</strong></p>
-                    <p>Temperatura: {item.main.temp}°C</p>
-                    <p>Temperatura minimalna: {item.main.temp_min}°C</p>
-                    <p>Temperatura maksymalna: {item.main.temp_max}°C</p>
-                    <p>Wilgotność: {item.main.humidity}%</p>
-                    <p>Zachmurzenie: {item.clouds.all}%</p>
-                    <p>Prędkość wiatru: {item.wind.speed}km/h</p>
-                    {item.weather[0].icon && (
-                    <img
-                        src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                        alt={item.weather[0].description}
-                    />
-                    )}
-                </div>
+                    <>
+                        <div key={index} className={`weather-container ${styles.details_all}`}>
+                            <p><strong>{item.weather[0].description}</strong></p>
+                            <p>Temperatura minimalna: {item.main.temp_min}°C</p>
+                            <p>Temperatura maksymalna: {item.main.temp_max}°C</p>
+                            <p>Wilgotność: {item.main.humidity}%</p>
+                            <p>Zachmurzenie: {item.clouds.all}%</p>
+                            <p>Prędkość wiatru: {item.wind.speed}km/h</p>
+                        </div>
+                        <div className={`${styles.details_main}`}>
+                            <h2>Pogoda {forecast.city.name}</h2>
+                            {item.weather[0].icon && (
+                                <img
+                                    src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                                    alt={item.weather[0].description}
+                                />
+                            )}
+                            <p>Temperatura: {item.main.temp}°C</p>
+                        </div>
+                    </>
                 ))}
             </div>
             </div>
