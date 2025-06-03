56b774b62774ff762942dacbfd5b934ae7e9e3b2
Piotr Kochanka <kochankap@gmail.com>
2025-06-03 23:14:26 +0200

    Improved styling

--- src/components/WeatherDetails/WeatherDetails.tsx
+++ src/components/WeatherDetails/WeatherDetails.tsx
@@ -22,22 +22,25 @@ const WeatherDetails: React.FC = () => {
                 {forecast.list.slice(0, 1).map((item, index) => (
                     <>
                         <div key={index} className={`weather-container ${styles.details_all}`}>
-                            <p><strong>{item.weather[0].description}</strong></p>
-                            <p>Temperatura minimalna: {Math.round(item.main.temp_min)}°C</p>
-                            <p>Temperatura maksymalna: {Math.round(item.main.temp_max)}°C</p>
-                            <p>Wilgotność: {item.main.humidity}%</p>
-                            <p>Zachmurzenie: {item.clouds.all}%</p>
-                            <p>Prędkość wiatru: {item.wind.speed}km/h</p>
-                        </div>
-                        <div className={`${styles.details_main}`}>
-                            <h2>Pogoda {forecast.city.name}</h2>
-                            {item.weather[0].icon && (
-                                <img
-                                    src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                                    alt={item.weather[0].description}
-                                />
-                            )}
-                            <p>Temperatura: {Math.round(item.main.temp)}°C</p>
+                            <div className={`${styles.details_main}`}>
+                                <h2>Pogoda {forecast.city.name}</h2>
+                                <p><strong>{item.weather[0].description}</strong></p>
+                                {item.weather[0].icon && (
+                                    <img
+                                        src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                                        alt={item.weather[0].description}
+                                    />
+                                )}
+                                <p>Temperatura: {Math.round(item.main.temp)}°C</p>
+                            </div>
+                            <div className={`${styles.details_all_temp}`}>
+                                
+                                <p>Temperatura minimalna: {Math.round(item.main.temp_min)}°C</p>
+                                <p>Temperatura maksymalna: {Math.round(item.main.temp_max)}°C</p>
+                                <p>Wilgotność: {item.main.humidity}%</p>
+                                <p>Zachmurzenie: {item.clouds.all}%</p>
+                                <p>Prędkość wiatru: {item.wind.speed}km/h</p> 
+                            </div>
                         </div>
                     </>
                 ))}
