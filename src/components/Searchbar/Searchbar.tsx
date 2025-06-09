0b88f98a85fa8e72ff21b7a9cabc4f0785e64d5a
Piotr Kochanka <kochankap@gmail.com>
2025-06-09 21:37:14 +0200

    Added the ability to search for weather by entering the city name in the search engine

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import styles from './searchbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { CityContext } from '../../CityContext';
 
 interface SearchbarProps{
     // onSearch: (query: string) => void;
@@ -9,17 +10,49 @@ interface SearchbarProps{
 }
 
 const Searchbar: React.FC<SearchbarProps> = ({ placeholder = 'Wpisz miasto...' }) => {
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const cityContext = useContext(CityContext);
+
+    if(!cityContext){
+        throw new Error('SearchBar must be used within a CityProvider');
+    }
+
+    const { setCity } = cityContext;
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSearch = () => {
+        if (searchTerm.trim() !== '') { 
+            setCity(searchTerm.trim()); 
+            setSearchTerm(''); 
+        }
+    };
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
     return(
-        <>
+        <div className={styles.searchbar_container}> {/* Dodaj kontener dla stylów */}
             <input
                 type="text"
                 placeholder={placeholder}
-                className={`${styles.searchbar}`}
+                className={styles.searchbar} // Upewnij się, że masz styl dla inputa
+                value={searchTerm} // Połącz wartość inputa ze stanem
+                onChange={handleInputChange} // Obsługa zmian w inputcie
+                onKeyPress={handleKeyPress} // Obsługa Enter
             />
-            <button className={`${styles.searchbar_button}`}>
+            <button
+                className={styles.searchbar_button}
+                onClick={handleSearch} // Dodaj obsługę kliknięcia przycisku
+            >
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
-        </>
+        </div>
     );
 }
 
